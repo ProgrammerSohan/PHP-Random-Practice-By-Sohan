8cf3501e025e97fda37a98e36105f4204f9eb49e
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-11-01 23:01:21 +0600

    1.1_array_a

---
+++ topic-arrays/index1.php
@@ -0,0 +1,5 @@
+<?php 
+    $authors = array("Steinbeck", "Kafka", "Tolkien","Dickens");
+    echo  " All Authors " . $authors[0] . ", " .$authors[1] . " ," . $authors[2] . " and " . $authors[3]. ".";
+
+?>
